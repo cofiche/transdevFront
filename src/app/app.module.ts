a4b2fae721566b566bb93e34d66c1909bd2d7aa7
Abderrahim TALEB <abderra.taleb@gmail.com>
2023-03-30 06:30:25 +0200

    front task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { UpdateReservationComponent } from './update-reservation/update-reservation.component';
+import { AddReservationComponent } from './add-reservation/add-reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationListComponent,
+    UpdateReservationComponent,
+    AddReservationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
